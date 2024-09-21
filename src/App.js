a8ceb1468bc7653a9f5885693bb190c87cf6d004
unknown <harshchavan1812@gmail.com>
2024-09-21 17:21:38 +0530

    header

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <Header />
+      </div>
+      <div className="app__body">
+        <div className="app__container">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
